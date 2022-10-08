05052deda7efac106948e0dcdb566563d066f29f
fierceray <92417450+fierceray@users.noreply.github.com>
2022-10-08 13:03:01 -0400

    update userdata.sh
    Modified to run

--- userdata.sh
+++ userdata.sh
@@ -2,12 +2,14 @@
 #install apache
 sudo apt update -y
 sudo apt upgrade -y
+#install package
+sudo apt install python3-pip -y
 sudo apt install apache2 -y
-#download website
+sudo apt-get install libapache2-mod-wsgi-py3 -y
 sudo git clone https://github.com/fierceray/tindog.git
 sudo mv tindog app
-sudo mv app/000-default.conf /etc/apache2/sites-enabled/
-sudo mv app /var/www/html/
-#run apache
-sudo systemctl start apache2
+sudo pip3 install -r app/requirements.txt 
+sudo ln -s /app /var/www/html/app
+sudo cp /app/000-default.conf /etc/apache2/sites-enabled/
+sudo systemctl restart apache2
 sudo systemctl enable apache2
